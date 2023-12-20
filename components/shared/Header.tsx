87175dd6c47f249229a403f6851129e8eb128894
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-12-20 12:07:52 +0800

    add Homepage Header

---
+++ components/shared/Header.tsx
@@ -0,0 +1,50 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
+
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+        <div className="wrapper flex-between">
+            <Link
+                href='/'
+                className="w-36 font-bold text-xl"
+            >
+                Gathxy
+            </Link>
+
+            {/* Navbar */}
+            <SignedIn>
+                <nav className="md:flex-between hidden w-full max-w-xs">
+                    <NavItems />
+                </nav>
+            </SignedIn>
+
+            <div className="flex w-32 justify-end gap-3">
+                {/* Clerk Setup */}
+                {/* Login */}
+                <SignedOut>
+                    <Button asChild className="rounded-full" size='lg' variant={'secondary'}>
+                        <Link
+                            href='/sign-in'
+                        >
+                            Login
+                        </Link>
+                    </Button>
+                </SignedOut>
+
+                {/* Logout */}
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/"/>
+                    <MobileNav />
+                </SignedIn>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
