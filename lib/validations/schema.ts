22e219915753849d82aa2d50318d600d19529eed
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-12-24 11:00:16 +0800

    update - event form schema validations

--- lib/validations/schema.ts
+++ lib/validations/schema.ts
@@ -3,16 +3,18 @@
 import * as z from "zod"
  
 export const EventFormSchema = z.object({
-  title: z.string().min(3, 'Title must be at least 3 characters'),
-  description: z.string().min(3, 'Description must be at least 3 characters').max(400, 'Description must less than 400 characters'),
-  location: z.string().min(3, 'Location must be at least 3 characters').max(400, 'Location must less than 400 characters'),
+  title: z.string().min(3, 'Please provide a title for the event.'),
+  description: z.string().min(3, 'Please provide the event descriptions.').max(400, 'Your description was way too long...'),
+  location: z.string().min(3, 'Invalid Location. Eg. California').max(400, 'Invalid Location. Eg. California'),
   imageUrl: z.string(),
   startDateTime: z.date(),
   endDateTime: z.date(),
   categoryId: z.string(),
   price: z.string(),
   isFree: z.boolean(),
-  url: z.string().url(),
+  url: z.string().url({
+    message: 'Invalid Url. Eg. https://eventurl.com'
+  }),
 })
 
 export type EventFormSchemaType = z.infer<typeof EventFormSchema>
