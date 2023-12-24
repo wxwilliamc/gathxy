20a889394b4a5051c95feae7a8b738ddfd59eb86
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-12-24 11:00:47 +0800

    update - uploadthing config

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,5 @@
 import { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateComponents } from "@uploadthing/react";
- 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+import { generateReactHelpers } from "@uploadthing/react/hooks";
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
