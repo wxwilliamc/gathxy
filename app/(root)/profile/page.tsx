3cbb0abdc98af5a53c367a44d69cf600fc3a951c
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-12-27 10:52:59 +0800

    update: List Purchased Orders

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,24 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventById, getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const MyProfilePage = async () => {
+const MyProfilePage = async ({ searchParams }: SearchParamProps) => {
+
+    const ordersPage = Number(searchParams?.ordersPage) || 1
+    const eventsPage = Number(searchParams?.eventsPage) || 1
 
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
-    const eventOrganizer = await getEventsByUser({ userId, page: 1})
+    const eventOrganizer = await getEventsByUser({ userId, page: eventsPage })
+
+    const orders = await getOrdersByUser({ userId, page: ordersPage })
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
 
     return (
         <>
@@ -28,18 +37,18 @@ const MyProfilePage = async () => {
                 </div>
             </section>
 
-            {/* <section className='wrapper my-8'>
+            <section className='wrapper my-8'>
                 <Collection
-                    data={[]}
+                    data={orderedEvents}
                     emptyTitle="No events tickets purchased yet"
                     emptySubTitle="Be patient, more events coming soon"
                     collectionType="My_Tickets"
                     limit={3}
-                    page={1}
-                    totalPages={2}
+                    page={ordersPage}
+                    totalPages={orders?.totalPages}
                     urlParamName='ordersPage'
                 />
-            </section> */}
+            </section>
 
             {/* Events Organized */}
             <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
@@ -63,8 +72,8 @@ const MyProfilePage = async () => {
                     emptySubTitle="Lets create some new events"
                     collectionType="Events_Organized"
                     limit={6}
-                    page={1}
-                    totalPages={2}
+                    page={eventsPage}
+                    totalPages={eventOrganizer?.totalPages}
                     urlParamName='eventsPage'
                 />
             </section>
